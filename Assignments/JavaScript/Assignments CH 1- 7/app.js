c016b328fc686e04bb5e0758394d0a2a18eef2ec
NabeelAhmedArain <nabeelahmedarain@gmail.com>
2023-08-07 14:25:15 +0500

    Started

---
+++ Assignments/JavaScript/Assignments CH 1- 7/app.js
@@ -0,0 +1,122 @@
+// Chapter 1
+// Question 1
+
+//alert("Welcome to my Website!");
+
+// Question 2
+// alert("Error! Please enter a valid password.");
+
+// Question 3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// Question 4 incomplete
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// Chapter 2
+// Question 1
+// var userName;
+
+// Question 2
+// var myName = "Nabeel Ahmed";
+
+// Question 3
+// var message = "Hello World";
+// alert(message);
+
+// Question 4
+// var studentData = prompt("Enter Your Name");
+// alert(studentData);
+
+// Question 5
+// var one = "Pizza\nPizz\nPiz\nPi\nP";
+// alert(one);
+
+// Chapter 3
+// Question 1
+// var age = 26;
+// alert(age);
+
+// Chapter 4
+// Question 1
+// var one, two, three;
+
+// Question 2
+// var firstName, num1, _num2, $name, flight_two; // Legal Variables Names
+// var &firstName, 67Home, -home, var, First-Name; // Illegal Variable Names
+
+// Question 3 A
+// document.write("<h1>" + "Rules for naming JS variables" + "</h1>"); 
+
+// Question 3 B
+// Underscore (_), Dollar($), alphabets(abc), number(home875765)
+
+// Question 3 C
+// Underscore(_), Dollar($), alphates(abc)
+
+// Question 3 D
+// case sensitive
+
+// Question 3 E
+// JS Keywords
+
+// Chapter 5-7
+// Question 1
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+// var sum = num1 + num2;
+// alert("Sum of " + num1 + " and " + num2 + " is " + sum);
+
+// Question 2 Subtraction
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+// var sub = num1 - num2;
+// alert("Result of subtracting " + num2 + " from " + num1 + " is " + sub);
+
+// Question 2 Multiplication
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+// var multiply = num1 * num2;
+// alert("Result of multiplying " + num1 + " and " + num2 + " is " + multiply);
+
+// Question 2 Division
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+// var divide = num1 / num2;
+// alert("Result of dividing " + num1 + " by " + num2 + " is " + divide);
+
+// Question 2 Modulus
+// var num1 = +prompt("Enter number 1");
+// var num2 = +prompt("Enter number 2");
+// var mod = num1 % num2;
+// alert("Remainder of dividing " + num1 + " by " + num2 + " is " + mod);
+
+// Question 3 
+// var num1; // a
+// document.write("Value after variable declaration is: " + num1); //b
+// num1 = 3; // c
+// document.write("<br>");
+// document.write("Initial value: " + num1); // d
+// ++num1; // e
+// document.write("<br>");
+// document.write("Value after increment is: " + num1); // f
+// num1 += 7; // g
+// document.write("<br>");
+// document.write("Value after addition is: " + num1); // h
+// --num1; // i
+// document.write("<br>");
+// document.write("Value after decrement is: " + num1); // j
+// document.write("<br>");
+// document.write("The remainder is: " + num1 % 3); // k
+
+// Question 4
+// var ticketCost = 600;
+// var totalTickets = 5;
+// document.write("Total cost to buy " + totalTickets + " tickets to a movie is " + ticketCost * totalTickets + " PKR");
+
+// Question 5
+// var anyNumber = +prompt("Enter a number for table");
+// document.write("Multiplication table of " + anyNumber + "<br>" + "<br>");
+// for (let i = 1; i <= 10; i++) {
+//     document.write(anyNumber + " x " + i + " = " + anyNumber * i + "<br>");
+// }
